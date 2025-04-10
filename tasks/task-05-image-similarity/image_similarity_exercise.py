6335ccda39bdd7835edfc7780b2ea961f624cc30
Ycaro Sales <ycaro00287@gmail.com>
2025-04-09 23:46:51 -0300

    other tasks

--- tasks/task-05-image-similarity/image_similarity_exercise.py
+++ tasks/task-05-image-similarity/image_similarity_exercise.py
@@ -31,6 +31,64 @@ Assume that i1 and i2 are normalized grayscale images (values between 0 and 1).
 
 import numpy as np
 
+
+def mean_squared_error(i1: np.ndarray, i2: np.ndarray) -> float:
+    squared_errors = (i1 - i2) ** 2
+    mean = np.mean(squared_errors)
+
+    return float(mean)
+
+
+def peak_signal_to_noise_ratio(i1: np.ndarray, i2: np.ndarray) -> float:
+    log10 = np.emath.log10
+    max_i1 = np.max(i1)
+    mse = mean_squared_error(i1, i2)
+
+    psnr = 20 * log10(max_i1) - 10 * log10(mse)
+
+    return psnr
+
+
+def structural_similarity_index(
+    i1: np.ndarray, i2: np.ndarray, k1: float = 0.01, k2: float = 0.03, L=1
+) -> float:
+    mean_i1 = np.mean(i1)
+    mean_i2 = np.mean(i2)
+
+    sm_i1 = mean_i1**2
+    sm_i2 = mean_i2**2
+
+    var_i1 = np.var(i1)
+    var_i2 = np.var(i2)
+
+    covar_i1_i2 = np.cov(i1.flatten(), i2.flatten())[0][1]
+
+    c1 = (k1 * L) ** 2
+    c2 = (k2 * L) ** 2
+
+    numerator = ((2 * mean_i1 * mean_i2) + c1) * ((2 * covar_i1_i2) + c2)
+    denominator = (sm_i1 + sm_i2 + c1) * (var_i1 + var_i2 + c2)
+
+    ssim = numerator / denominator
+
+    return ssim
+
+
+def normalized_pearson_correlation_coefficient(i1: np.ndarray, i2: np.ndarray) -> float:
+    covar_i1_i2 = np.cov(i1.flatten(), i2.flatten())[0][1]
+    std_i1 = np.std(i1)
+    std_i2 = np.std(i2)
+    pcc = covar_i1_i2 / (std_i1 * std_i2)
+
+    npcc = (1 / 255) * pcc
+
+    return npcc
+
+
 def compare_images(i1: np.ndarray, i2: np.ndarray) -> dict:
-    # Your implementation here
-    pass
+    mse: float = mean_squared_error(i1, i2)
+    psnr: float = peak_signal_to_noise_ratio(i1, i2)
+    ssim: float = structural_similarity_index(i1, i2)
+    npcc: float = normalized_pearson_correlation_coefficient(i1, i2)
+
+    return {"mse": mse, "psnr": psnr, "ssim": ssim, "npcc": npcc}
--- tasks/task-06-geometric-transformations/image_geometry_exercise.py
+++ tasks/task-06-geometric-transformations/image_geometry_exercise.py
@@ -29,6 +29,112 @@ The return value should be like:
 
 import numpy as np
 
+def create_bilinear_interpolation_function(img: np.ndarray)-> np.ufunc:
+    
+    def bilinear_interpolation_function(value: int, indexes: tuple) -> int:
+        x,y = indexes
+        A = np.zeros(shape=(4,))
+        neighbor_sum = [[1,0],[-1,0],[0,1],[0,-1]]
+
+        pixel_neighbors = [[x,y],[x,y],[x,y],[x,y]]
+
+        for index, value in enumerate(neighbor_sum):
+            pixel_neighbors[
+            
+            
+        
+
+
+        interpolated_value = 
+        return interpolated_value
+    return np.frompyfunc(bilinear_interpolation_function, 2, 1)
+
+
+def translate_image(img: np.ndarray, shift_x: int = 1, shift_y: int = 1) -> np.ndarray:
+    affine_matrix = [[shift_x, 0, 0], [0, shift_y, 0], [0, 0, 1]]
+
+    trans_img = np.zeros(shape=(img.shape), dtype=img.dtype)
+
+    for (i, j), value in np.ndenumerate(img):
+        input_coords = np.array([i, j, 1])
+        i_out, j_out, _ = input_coords @ affine_matrix
+
+        if 0 <= i_out <= img.shape[0] - 1 or 0 <= j_out <= img.shape[1] - 1:
+            trans_img[i_out, j_out] = value
+        else:
+            pass
+
+    return img
+
+
+def rotate_image_90_degrees(img: np.ndarray, rotations=1) -> np.ndarray:
+    cos = np.cos((np.pi / 2) * rotations)
+    sin = np.sin((np.pi / 2) * rotations)
+
+    affine_matrix = [[cos, sin, 0], [-sin, cos, 0], [0, 0, 1]]
+
+    img_rotated = np.zeros(shape=img.shape, dtype=img.dtype)
+
+    for (i, j), value in np.ndenumerate(img):
+        input_coords = np.array([i, j, 1])
+        i_out, j_out, _ = input_coords @ affine_matrix
+        if 0 <= i_out <= img.shape[0] - 1 or 0 <= j_out <= img.shape[1] - 1:
+            img_rotated[i_out, j_out] = value
+        else:
+            pass
+
+    return img_rotated
+
+
+def stretch_image(
+    img: np.ndarray, scalar_x: float = 1.5, scalar_y: float = 1.0
+) -> np.ndarray:
+    img_stretched = np.zeros(shape=img.shape, dtype=np.uint8)
+
+    affine_matrix = [[scalar_x, 0, 0], [0, scalar_y, 0], [0, 0, 1]]
+
+    for (i, j), value in np.ndenumerate(img):
+        input_coords = np.ndarray([i, j, 1])
+        i_out, j_out, _ = input_coords @ affine_matrix
+        if 0 <= i_out <= img.shape[0] - 1 or 0 <= j_out <= img.shape[1] - 1:
+            img_stretched[i_out, j_out] = value
+        else:
+            pass
+
+    return img_stretched
+
+
+def mirror_image_horizontally(img: np.ndarray) -> np.ndarray:
+    mirror_img = np.empty(shape=img.shape, dtype=np.uint8)
+
+    affine_matrix = [[-1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+    for (i, j), value in np.ndenumerate(img):
+        input_coords = np.ndarray([i, j, 1])
+        i_out, j_out, _ = input_coords @ affine_matrix
+        if 0 <= i_out <= img.shape[0] - 1 or 0 <= j_out <= img.shape[1] - 1:
+            mirror_img[i_out, j_out] = value
+        else:
+            pass
+
+    return mirror_img
+
+
+def barrel_distort_image(img: np.ndarray) -> np.ndarray:
+    return
+
+
 def apply_geometric_transformations(img: np.ndarray) -> dict:
-    # Your implementation here
-    pass
+    translated_img = translate_image(img)
+    mirrored_img = mirror_image_horizontally(img)
+    distorted_img = barrel_distort_image(img)
+    rotated_img = rotate_image_90_degrees(img)
+    stretched_img = stretch_image(img)
+
+    return {
+        "translated": translated_img,
+        "rotated": rotated_img,
+        "stretched": stretched_img,
+        "mirrored": mirrored_img,
+        "distorted": distorted_img,
+    }
